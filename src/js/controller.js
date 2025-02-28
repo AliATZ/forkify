f954323157b3902ad52ebf4b9bc22b5a0988e97a
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-02-28 13:39:19 +0330

    started

---
+++ src/js/controller.js
@@ -0,0 +1,28 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+
+///////////////////////////////////////
+
+async function showRecipe (){
+  try {
+    const res = fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+
+    const data = res.json()
+    console.log(res,data)
+  }catch (err){
+    alert(err)
+  }
+}
+
+showRecipe()
+
